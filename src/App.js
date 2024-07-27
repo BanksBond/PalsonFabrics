a34ef87701d1778cea71e72014117463445fa424
Bakhtyar Ansari <bakhtyaransari1@gmail.com>
2024-07-27 12:50:50 +0530

    2nd

--- src/App.js
+++ src/App.js
@@ -1,74 +1,66 @@
 import { Col, Nav, Navbar, Row, Button } from "react-bootstrap";
 import Carousel from "react-bootstrap/Carousel";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// eslint-disable-next-line no-unused-vars
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import FabricProducts from "./components/FabricProducts";
 import ProductDetails from "./components/ProductDetails";
 import products from "./data/products";
+import { useState } from "react";
 
 export default function App() {
+  const [page, setPage] = useState("home");
+
+  function handleSelectPage(p) {
+    setPage(p);
+  }
+
+  return (
+    <Routes>
+      <Route
+        exact
+        path="/"
+        element={<Home page={page} onPage={handleSelectPage} />}
+      />
+      <Route
+        path="/products"
+        element={
+          <section id="products" className="container-fluid">
+            <FabricProducts
+              products={products}
+              page={page}
+              onPage={handleSelectPage}
+            />
+          </section>
+        }
+      />
+      <Route
+        path="/product/:id"
+        element={
+          <section id="product-details" className="container-fluid">
+            <ProductDetails products={products} />
+          </section>
+        }
+      />
+    </Routes>
+  );
+}
+
+function Home({ page, onPage }) {
   return (
     <div>
-      <Section1 />
+      <Section1 page={page} onPage={onPage} />
       <SectionFeature />
       <SectionCarousel />
-      <SectionProducts />
-      {/* <div className="container-fluid">
-        <Router>
-          <Routes>
-            <Route path="/" element={<FabricProducts products={products} />} />
-            <Route
-              path="/product/:id"
-              element={<ProductDetails products={products} />}
-            />
-          </Routes>
-        </Router>
-      </div> */}
-
       <SectionSample />
+      <Footer />
     </div>
   );
 }
 
-function SectionProducts() {
-  return (
-    <section className="container-fluid">
-      <Router>
-        <Routes>
-          <Route path="/" element={<FabricProducts products={products} />} />
-          <Route
-            path="/product/:id"
-            element={<ProductDetails products={products} />}
-          />
-        </Routes>
-      </Router>
-    </section>
-  );
-}
-
-function Section1() {
+function Section1({ page, onPage }) {
   return (
     <section id="title" className="container-fluid">
-      <Navbar
-        expand={"lg"}
-        className=" mb-3"
-        bg="transparent"
-        data-bs-theme="dark"
-      >
-        <Navbar.Brand className="fs-1" href="#">
-          Palson Fabrics
-        </Navbar.Brand>
-
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="justify-content-end flex-grow-1 pe-3 fs-5">
-            <Nav.Link active href="#home">
-              Home
-            </Nav.Link>
-            <Nav.Link href="#features">Products</Nav.Link>
-            <Nav.Link href="#sample">Contacts</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
+      <NavBar page={page} onPage={onPage} />
 
       <Row>
         <Col lg={"6"}>
@@ -80,7 +72,7 @@ function Section1() {
             Contact Us
           </Button>
           <Button className="download-button" variant="outline-light" size="lg">
-            <i class="fa-solid fa-phone"></i>
+            <i className="fa-solid fa-phone"></i>
             Contact Us
           </Button>
         </Col>
@@ -94,6 +86,48 @@ function Section1() {
         </Col>
       </Row>
     </section>
+  );
+}
+
+export function NavBar({ page, onPage }) {
+  return (
+    <Navbar
+      expand={"lg"}
+      className=" mb-3"
+      bg="transparent"
+      data-bs-theme="dark"
+    >
+      <Navbar.Brand className="fs-1" href="#">
+        Palson Fabrics
+      </Navbar.Brand>
+
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="justify-content-end flex-grow-1 pe-3 fs-5">
+          <Link
+            to="/"
+            className={`${page === "home" ? "active" : ""} nav-link`}
+            onClick={() => onPage("home")}
+          >
+            Home
+          </Link>
+          {/* <Nav.Link active href="/">
+            Home
+          </Nav.Link> */}
+          {/* <Nav.Link href="/products">Products</Nav.Link> */}
+          <Link
+            onClick={() => onPage("products")}
+            to="/products"
+            className={`${page === "products" ? "active" : ""} nav-link`}
+          >
+            Products
+          </Link>
+          <a href="#sample" className="nav-link">
+            Contacts
+          </a>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
 }
 
@@ -111,7 +145,7 @@ function SectionFeature() {
           </p>
         </div>
         <div className="feat anim">
-          <i class="fa-solid fa-splotch fa-5x"></i>
+          <i className="fa-solid fa-splotch fa-5x"></i>
           <h3>Scotchguard (SG)</h3>
           <p>
             Scotchguard treatment helps to block stains like ketchup, tea,
@@ -192,9 +226,35 @@ function SectionSample() {
         variant="outline-light"
         size="lg"
       >
-        <i class="fa-solid fa-phone"></i>
+        <i className="fa-solid fa-phone"></i>
         Contact Us
       </Button>
     </section>
+  );
+}
+
+function Footer() {
+  const googleMapsUrl =
+    "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3500.746607847802!2d77.38235679616874!3d28.66730487648667!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390cf073fa88b4c1%3A0xc7bbc3c5544a51f3!2sGardenia%20Glamour%20Phase%201!5e0!3m2!1sen!2sin!4v1722064484783!5m2!1sen!2sin";
+  return (
+    <footer className="footer">
+      <div className="footer-container">
+        <div className="footer-info">
+          <p>&copy; 2024 Your Company Name</p>
+          <p>123 Your Street, Your City, Your Country</p>
+        </div>
+        <div className="footer-map">
+          <iframe
+            src={googleMapsUrl}
+            width="600"
+            height="450"
+            style={{ border: 0, borderRadius: "8px" }}
+            allowFullScreen
+            loading="lazy"
+            title="Google Maps"
+          ></iframe>
+        </div>
+      </div>
+    </footer>
   );
 }
--- src/components/FabricProducts.js
+++ src/components/FabricProducts.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import "./FabricProducts.css";
+import { NavBar } from "../App";
 
-const FabricProducts = ({ products }) => {
+const FabricProducts = ({ products, page, onPage }) => {
   const navigate = useNavigate();
 
   const handleProductClick = (id) => {
@@ -10,22 +11,28 @@ const FabricProducts = ({ products }) => {
   };
 
   return (
-    <div className="fabric-products">
-      {products.map((product) => (
-        <div
-          key={product.id}
-          className="fabric-product"
-          onClick={() => handleProductClick(product.id)}
-        >
-          <h3>{product.name}</h3>
-          <img
-            src={product.image}
-            alt={product.name}
-            className="fabric-image"
-          />
-        </div>
-      ))}
-    </div>
+    <>
+      <section id="title">
+        <NavBar page={page} onPage={onPage} />
+      </section>
+      <h1>Products</h1>
+      <div className="fabric-products">
+        {products.map((product) => (
+          <div
+            key={product.id}
+            className="fabric-product"
+            onClick={() => handleProductClick(product.id)}
+          >
+            <h3>{product.name}</h3>
+            <img
+              src={product.image}
+              alt={product.name}
+              className="fabric-image"
+            />
+          </div>
+        ))}
+      </div>
+    </>
   );
 };
 
